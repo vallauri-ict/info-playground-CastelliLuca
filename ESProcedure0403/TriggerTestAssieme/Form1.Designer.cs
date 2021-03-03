ebbb0e343bc3a50925dbed38c3e8e02201c4a927
CastelliLuca <58586845+CastelliLuca@users.noreply.github.com>
2021-03-03 17:43:55 +0100

    03/03/2021 esercizio statistiche piloti

---
+++ ESProcedure0403/TriggerTestAssieme/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace TriggerTestAssieme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFunction1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnFunction1
+            // 
+            this.btnFunction1.Location = new System.Drawing.Point(12, 12);
+            this.btnFunction1.Name = "btnFunction1";
+            this.btnFunction1.Size = new System.Drawing.Size(159, 23);
+            this.btnFunction1.TabIndex = 3;
+            this.btnFunction1.Text = "Carica";
+            this.btnFunction1.UseVisualStyleBackColor = true;
+            this.btnFunction1.Click += new System.EventHandler(this.btnAggDB_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 260);
+            this.Controls.Add(this.btnFunction1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnFunction1;
+    }
+}
+
---
+++ ESProcedure0403/TriggerTestAssieme/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace TriggerTestAssieme
+{
+    public partial class Form1 : Form
+    {
+        public static readonly string workingDirectory = Environment.CurrentDirectory;
+        public const string PATH = @"C:\data\formulaoneBelly\";
+        public const string CONNECTION_STRING = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + PATH + "formulaone.mdf;Integrated Security=True";
+
+        private void btnAggDB_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(CONNECTION_STRING);
+            con.Open();
+            string sql = "Procedure";
+
+            SqlCommand cmd = new SqlCommand(sql, con);
+            SqlParameter data = new SqlParameter();
+            data.ParameterName = "data";
+            data.Direction = ParameterDirection.Input;
+            data.DbType = DbType.DateTime;
+            data.Value = "04/10/2002";
+            cmd.Parameters.Add(data);
+
+            cmd.CommandText = "Procedure";
+
+            SqlDataReader reader = cmd.ExecuteReader();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
